83347f45a9d5fdd0fb98b55a758262fd925a7810
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-07-06 13:07:12 -0300

    useState

---
+++ src/TaskList.jsx
@@ -0,0 +1,15 @@
+import { tasks } from './tasks'
+import { useState } from 'react'
+
+console.log(tasks)
+
+function TaskList() {
+   
+  const [first, setfirst] = useState(second)
+
+  return (
+    <div> TaskList</div>
+  )
+}
+
+export default TaskList
---
+++ src/tasks.js
@@ -0,0 +1,18 @@
+ export const tasks = [ 
+    { 
+        id: 0,
+        title:"mi primer tarea",
+        description:"mi primer tarea",
+    },
+    { 
+        id: 0,
+        title:"mi segunda tarea",
+        description:"mi segunda tarea",
+    },
+    { 
+        id: 0,
+        title:"mi tercer tarea",
+        description:"mi tercer tarea",
+    },
+];
+
